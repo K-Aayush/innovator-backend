3ef5bce7825b934b66c7dcabebe962c9725086a3
Aayush <ayushkarkee6@gmail.com>
2025-06-29 16:41:57 +0545

    course content methods

--- src/modules/courses/course.routes.js
+++ src/modules/courses/course.routes.js
@@ -3,11 +3,44 @@ const AdminFiles = require("../../utils/fileProcessor/multer.courses");
 const { AddCourse, DelCourses } = require("./course.admin.methods");
 const { MultipleFiles, DeleteFiles } = require("./course.file");
 const ListCourses = require("./courses.list");
-// const { GetCourse } = require("./courses.methods");
+const {
+  GetCoursePDFs,
+  GetCourseVideos,
+  DownloadPDF,
+  DownloadVideo,
+} = require("./course.content.methods");
+const {
+  CreateCourseCategory,
+  GetCourseCategories,
+  GetCoursesByCategory,
+  UpdateCourseCategory,
+  DeleteCourseCategory,
+  GetCategoryHierarchy,
+} = require("./course.category.methods");
 
 const route = require("express").Router();
 
-// add files
+// Course Category Management (Admin only)
+route.post("/admin/course-categories", basicMiddleware, CreateCourseCategory);
+route.get("/course-categories", GetCourseCategories);
+route.get("/course-categories/hierarchy", GetCategoryHierarchy);
+route.get(
+  "/course-categories/:categoryId/courses",
+  basicMiddleware,
+  GetCoursesByCategory
+);
+route.put(
+  "/admin/course-categories/:categoryId",
+  basicMiddleware,
+  UpdateCourseCategory
+);
+route.delete(
+  "/admin/course-categories/:categoryId",
+  basicMiddleware,
+  DeleteCourseCategory
+);
+
+// File uploads
 route.post(
   "/add-public-course-file",
   basicMiddleware,
@@ -23,18 +56,28 @@ route.post(
 
 route.delete("/delete-course-files", basicMiddleware, DeleteFiles);
 
+// Course management
 route.post("/add-course", basicMiddleware, AddCourse);
-
-// delete courses
 route.delete("/delete-courses/:id", basicMiddleware, DelCourses);
 
-// list courses
+// List courses
 route.get("/list-courses", basicMiddleware, ListCourses);
-// admin list courses
 route.get("/admin-list-courses/:page", basicMiddleware, ListCourses);
 
-// admin individual
-// route.get("/get-course/:id", basicMiddleware, GetCourse);
-// route.get("/admin-get-course/:id", basicMiddleware, GetCourse);
+// Course content routes - PDFs and Videos separately
+route.get("/courses/:courseId/pdfs", basicMiddleware, GetCoursePDFs);
+route.get("/courses/:courseId/videos", basicMiddleware, GetCourseVideos);
+
+// Download routes
+route.get(
+  "/courses/download-pdf/:courseId/:noteId",
+  basicMiddleware,
+  DownloadPDF
+);
+route.get(
+  "/courses/download-video/:courseId/:noteId",
+  basicMiddleware,
+  DownloadVideo
+);
 
 module.exports = route;
